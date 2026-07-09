f5d624e0ea3e873e2ed5cde1ed9f572c6db01040
Lucca Seabra Cruz <lseabra@c1r12s7.42porto.com>
2026-07-09 13:33:28 +0100

    fix: remove unused variable sufix_len

--- module7/ex03/append_it.py
+++ module7/ex03/append_it.py
@@ -6,7 +6,6 @@ if (len(sys.argv) < 2):
     print("none")
     sys.exit()
 sufix = "ism"
-sufix_len = len(sufix)
 for arg in sys.argv[1:]:
     arg_len = len(arg)
     if arg_len > 0 and arg.find(sufix, -3) == -1:
